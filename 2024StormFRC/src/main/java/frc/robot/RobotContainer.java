f1081eaf19d19057ace7253239575d382456e608
stormrobotics8849 <stormrobotics8849@gmail.com>
2024-03-26 16:02:55 -0400

    akarsh's shitty ass code

--- 2024StormFRC/src/main/java/frc/robot/RobotContainer.java
+++ 2024StormFRC/src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -108,17 +109,17 @@ public class RobotContainer {
 
   }
 
-  public void registerNamedCommands() {
-    NamedCommands.registerCommand("timedIntake and timed shoot", new SequentialCommandGroup(new TimedIntake(manipulator),new AutoShoot(manipulator,Constants.autoShootingVelocity)));
-  }
+
 
   public RobotContainer() {
+    NamedCommands.registerCommand("timedIntake", new ParallelCommandGroup(new TimedIntake(manipulator), new TimedShoot(manipulator)) );
+    // NamedCommands.registerCommand("timedShoot", new TimedShoot(manipulator));
 
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
     configureBindings();
 
-    registerNamedCommands();
+   
   }
 
   public Command getAutonomousCommand() {
